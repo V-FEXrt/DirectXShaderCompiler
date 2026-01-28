ad7268c006d6d58209768078c1feb6ebac98810d
Ashley Coleman <ascoleman@microsoft.com>
2026-01-27 18:04:36 -0700

    [SM6.10] Implement FillMatrix Builtin

---
+++ tools/clang/test/SemaHLSL/hlsl/linalg/builtins/nominal.hlsl
@@ -0,0 +1,10 @@
+// REQUIRES: dxil-1-10
+// RUN: %dxc -T cs_6_10 -E main %s -verify
+
+// expected-no-diagnostics
+
+[numthreads(1,1,1)]
+void main() {
+  __builtin_LinAlg_Matrix mat;
+  __builtin_LinAlg_FillMatrix(mat, 5);
+}
