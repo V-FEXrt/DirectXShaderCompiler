52aff8fcfff8264673c7ce2376619aaa4ccb454c
Ashley Coleman <ascoleman@microsoft.com>
2026-02-02 13:14:27 -0700

    [SM6.10][NFC] Address readability comment in tests

--- tools/clang/test/SemaHLSL/hlsl/linalg/builtins/fillmatrix/ast.hlsl
+++ tools/clang/test/SemaHLSL/hlsl/linalg/builtins/fillmatrix/ast.hlsl
@@ -1,11 +1,11 @@
 // REQUIRES: dxil-1-10
 // RUN: %dxc -T lib_6_10 -E main %s -ast-dump-implicit | FileCheck %s
 
-// CHECK: FunctionDecl {{[^ ]+}} <<invalid sloc>> <invalid sloc> implicit used __builtin_LinAlg_FillMatrix 'void (__builtin_LinAlg_Matrix, unsigned int)' extern
-// CHECK-NEXT: ParmVarDecl {{[^ ]+}} <<invalid sloc>> <invalid sloc> res '__restrict __builtin_LinAlg_Matrix'
-// CHECK-NEXT: ParmVarDecl {{[^ ]+}} <<invalid sloc>> <invalid sloc> value 'unsigned int'
-// CHECK-NEXT: HLSLIntrinsicAttr {{[^ ]+}} <<invalid sloc>> Implicit "op" "" 406
-// CHECK-NEXT: AvailabilityAttr {{[^ ]+}} <<invalid sloc>> Implicit  6.10 0 0 ""
+// CHECK: FunctionDecl {{.*}} implicit used __builtin_LinAlg_FillMatrix 'void (__builtin_LinAlg_Matrix, unsigned int)' extern
+// CHECK-NEXT: ParmVarDecl {{.*}} res '__restrict __builtin_LinAlg_Matrix'
+// CHECK-NEXT: ParmVarDecl {{.*}} value 'unsigned int'
+// CHECK-NEXT: HLSLIntrinsicAttr {{.*}} Implicit "op" "" 406
+// CHECK-NEXT: AvailabilityAttr {{.*}} Implicit  6.10 0 0 ""
 
 [shader("compute")]
 [numthreads(1,1,1)]
--- tools/clang/test/SemaHLSL/hlsl/linalg/builtins/matrixloadfromdescriptor/ast.hlsl
+++ tools/clang/test/SemaHLSL/hlsl/linalg/builtins/matrixloadfromdescriptor/ast.hlsl
@@ -1,14 +1,14 @@
 // REQUIRES: dxil-1-10
 // RUN: %dxc -T lib_6_10 -E main %s -ast-dump-implicit | FileCheck %s
 
-// CHECK: FunctionDecl {{[^ ]+}} <<invalid sloc>> <invalid sloc> implicit used __builtin_LinAlg_MatrixLoadFromDescriptor 'void (__builtin_LinAlg_Matrix, RWByteAddressBuffer, unsigned int, unsigned int, unsigned int)' extern
-// CHECK-NEXT: ParmVarDecl {{[^ ]+}} <<invalid sloc>> <invalid sloc> res '__restrict __builtin_LinAlg_Matrix'
-// CHECK-NEXT: ParmVarDecl {{[^ ]+}} <<invalid sloc>> <invalid sloc> buf 'RWByteAddressBuffer'
-// CHECK-NEXT: ParmVarDecl {{[^ ]+}} <<invalid sloc>> <invalid sloc> offset 'unsigned int'
-// CHECK-NEXT: ParmVarDecl {{[^ ]+}} <<invalid sloc>> <invalid sloc> stride 'unsigned int'
-// CHECK-NEXT: ParmVarDecl {{[^ ]+}} <<invalid sloc>> <invalid sloc> layout 'unsigned int'
-// CHECK-NEXT: HLSLIntrinsicAttr {{[^ ]+}} <<invalid sloc>> Implicit "op" "" 410
-// CHECK-NEXT: AvailabilityAttr {{[^ ]+}} <<invalid sloc>> Implicit  6.10 0 0 ""
+// CHECK: FunctionDecl {{.*}} implicit used __builtin_LinAlg_MatrixLoadFromDescriptor 'void (__builtin_LinAlg_Matrix, RWByteAddressBuffer, unsigned int, unsigned int, unsigned int)' extern
+// CHECK-NEXT: ParmVarDecl {{.*}} res '__restrict __builtin_LinAlg_Matrix'
+// CHECK-NEXT: ParmVarDecl {{.*}} buf 'RWByteAddressBuffer'
+// CHECK-NEXT: ParmVarDecl {{.*}} offset 'unsigned int'
+// CHECK-NEXT: ParmVarDecl {{.*}} stride 'unsigned int'
+// CHECK-NEXT: ParmVarDecl {{.*}} layout 'unsigned int'
+// CHECK-NEXT: HLSLIntrinsicAttr {{.*}} Implicit "op" "" 410
+// CHECK-NEXT: AvailabilityAttr {{.*}} Implicit  6.10 0 0 ""
 
 RWByteAddressBuffer inbuf;
 
